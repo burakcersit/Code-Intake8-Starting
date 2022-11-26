8a98f119d2c3ebab47ffbe7f7188af6921b0e966
burakcersit <burakcersit1@gmail.com>
2022-11-26 18:04:49 -0500

    practices

--- JavaScript/basic.js
+++ JavaScript/basic.js
@@ -1,11 +1,27 @@
 
 
-let rectangle = 5 * 13
-console.log(rectangle)
+// let rectangle = 5 * 13
+// console.log(rectangle)
 
-let circle = Math.PI * 25 * 2
-console.log(circle)
+// let circle = Math.PI * 25 * 2
+// console.log(circle)
 
-let cylinder = Math.PI * 30**2 * 50
-console.log(cylinder)
+// let cylinder = Math.PI * 30**2 * 50
+// console.log(cylinder)
+
+
+// let a = 10
+// let b = 15
+// let c = 10
+// let d = 70 
+// let e = 105
+
+// let sum = a + b + c + d + e
+// console.log("my total number is", sum)
+
+// let average = sum / 5
+// console.log("my average is", average)
+
+// let per = (a/25)*100
+
 
