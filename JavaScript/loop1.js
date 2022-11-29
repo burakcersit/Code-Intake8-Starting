b77431468176cd42ed35fd8dc82d036bfc604d4c
burakcersit <burakcersit1@gmail.com>
2022-11-29 14:03:44 -0500

    practices

---
+++ JavaScript/loop1.js
@@ -0,0 +1,20 @@
+
+for(let i=0; i<=10; i++){
+    console.log("for loop", i)
+}
+
+
+
+let i = 0;
+while(i<=10){
+    console.log("while loop", i)
+    i++
+}
+
+
+let m = 0;
+do{
+    console.log("do while", m)
+  m++
+ }
+ while(m<=10)
