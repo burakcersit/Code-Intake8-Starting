b189c7cf817e0b3351329e525f0db2958e332b88
burakcersit <burakcersit1@gmail.com>
2022-11-30 12:08:20 -0500

    functions

---
+++ JavaScript/functions.js
@@ -0,0 +1,26 @@
+// block of code that performs a particular task 
+
+
+//function declaration
+// function name-of-function(param1, param2, ,,,,){
+//     //code
+// }
+
+// function display(){
+//     console.log("bob")
+// }
+// // function call invoke
+// display()
+
+// function display(name, surname){
+//     console.log("my full name is", name, surname)
+// }
+// display("burak", 'cersit')
+
+
+function cube(num){
+    let cube = num * num * num
+    console.log(cube)
+}
+
+cube(5)
