89a58ad623c64ddcd47a9ed66c7a481811c22655
burakcersit <burakcersit1@gmail.com>
2022-11-30 14:13:02 -0500

    practice

--- JavaScript/calculator.js
+++ JavaScript/calculator.js
@@ -28,9 +28,22 @@ function remainder(num1, num2){
     console.log("remainder is:", result)
 }
 
+//AREA OF RECTANGLE
+function arearectangle(length, width){
+    let result = length * width
+    console.log("area:", result)
+}
+
+//SQUARE OF A NUMBER
+function square(number){
+    console.log("squareis :", number*number)
+}
+
 //CALL FUNCTIONS
 addition(10,20)
 multiplication(10,20)
 division(10,20)
 subtrcation(10,20)
 remainder(100,20)
+arearectangle(10,50)
+square(10)
--- JavaScript/fp1.js
+++
@@ -1,5 +0,0 @@
-
-for (let i=1; i<=2; i++){
-    let maxnumbers = parseFloat(prompt("enter numbers to find max"))
-}
-console.log(maxnumbers)
