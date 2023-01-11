97427e1ba428d95ddebf1f5a4748a54b3eb92fb1
burakcersit <burakcersit@hotmail.com>
2023-01-11 10:02:34 -0500

    dlete

--- JavaScript/Express/index1.js
+++ JavaScript/Express/index1.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.set("view engine", "ejs")
-app.get('/', (req,res)=>{
-    res.send("home page")
-})
 
-app.get('/test', (req,res)=>
-res.render('test')
+// app.get('/', (req,res)=>{
+//     res.send("form")
+// })
+
+app.get('/', (req,res)=>
+res.render('form')
 )
+
+app.post('/save'), (req,res)=>{
+    res.send("form submitted")
+}
 
 app.listen(port,()=>
 console.log("server running on port 8080")
